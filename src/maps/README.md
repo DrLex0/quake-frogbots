5961567cad759a6cf1e6d457a853e63a38306f39
Alexander Thomas <doctor.lex@gmail.com>
2025-04-06 18:14:32 +0200

    Allow specifying aliases for maps
    For some reason, `trindm1.bsp` is exactly the same as `tridm1.bsp`, same for 2 and 3.

--- src/maps/README.md
+++ src/maps/README.md
@@ -15,19 +15,31 @@ To use a different maplist file than the default `maplist.txt`, specify it with 
 
 Because the format of waypoint files has changed compared to the older Frogbot code, waypoint files created with the older waypoint tool must be _transformed._ Do this by enabling the `-t` option of the script, it can't hurt to always enable this because it auto-detects the format.
 
-## Example
+## Aliases
 
-Re-generate the maplist file using all map QC files in this directory and the `trinca` subfolder:
-```bash
-./generate_maplist.py -vtl -d trinca
+It is possible to have _aliases_ for maps, for instance if `trindm2.bsp` and `blorkination.bsp` are the exact same map files as `tridm2.bsp`, then you can add this kind of line to the top of the `map_tridm2.qc` file:
+```
+// ALIASES trindm2 blorkination
 ```
 
-Render the QuakeC source code from the maplist file:
+The script will then automatically generate a `mapaliases.txt` file containing the following line, which will reuse the waypoints created for `tridm2` without having to copy-paste them:
+```
+tridm2 trindm2 blorkination
+```
+
+## Example
+
+Re-generate the maplist and aliases file using all map QC files in this directory and the `drlex, ktx, trinca` subfolders:
+```bash
+./generate_maplist.py -vtl -d drlex ktx trinca
+```
+
+Render the QuakeC source code from the maplist and aliases file:
 ```bash
 ./generate_maplist.py -vg
 ```
 
 Or, do all of the above in a single invocation:
 ```bash
-./generate_maplist.py -vtlg -d trinca
+./generate_maplist.py -vtlg -d drlex ktx trinca
 ```
--- src/maps/generate_maplist.py
+++ src/maps/generate_maplist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Generate the maplist and/or QuakeC code for v2 FrogBot.
 Also acts as a conversion tool for old waypoint files.
-2024-12/2025-02, Alexander Thomas aka DrLex.
+2024-12/2025-04, Alexander Thomas aka DrLex.
 
 Released under GPL license."""
 
@@ -19,8 +19,19 @@ COLUMNS = 3
 COL_WIDTH = 10
 
 
-def add_maps(maps: list[str], glob_pattern: str) -> None:
-    """Appends maps to the list, found according to the glob pattern."""
+def mapname_from_path(path: str) -> str:
+    """Returns lowercase "mapname" if path ends with a file name like 'map_mApNaMe.qc',
+    otherwise returns empty string."""
+    base = os.path.basename(path).lower()
+    if mat := re.match(r"map_(.*)\.qc$", base):
+        return mat.group(1)
+    return ""
+
+
+def add_maps(maps: list[str], aliases: dict[str, list[str]], glob_pattern: str) -> None:
+    """Appends map paths to the list, found according to the glob pattern.
+    Aliases will be extended with entries for the map name if the QC file
+    contains a line '// ALIASES alias1 alias2' etc."""
     for map_file in sorted(glob.glob(glob_pattern)):
         bad = []
         if any(ord(char) > 127 for char in map_file):
@@ -35,28 +46,80 @@ def add_maps(maps: list[str], glob_pattern: str) -> None:
             print(f"WARNING: skipping '{map_file}' because it contains: {', '.join(bad)}",
                   file=sys.stderr)
             continue
+        map_name = mapname_from_path(map_file)
+        if not map_name:
+            print(f"WARNING: skipping '{map_file}' because invalid file name (should not happen!)",
+                  file=sys.stderr)
+            continue
+        if [True for path in maps if mapname_from_path(path) == map_name]:
+            print(f"WARNING: skipping '{map_file}' because duplicate map name", file=sys.stderr)
+            continue
+
         maps.append(map_file)
+        with open(map_file, encoding="iso8859_15") as map_data:
+            for line in map_data.readlines():
+                if mat := re.match(r"//\s*ALIASES (.*)\s*$", line):
+                    ali = re.split(r"\s+", mat.group(1))
+                    if ali:
+                        aliases[map_name] = ali
+                    break
 
 
 def generate_listfile(args: argparse.Namespace) -> None:
-    """Writes a map sources list file named args.list_file."""
+    """Writes a map sources list file named args.list_file and an aliases file
+    named args.alias_file."""
     maps: list[str] = []
-    add_maps(maps, "map_*.qc")
+    aliases: dict[str, list[str]] = {}
+    add_maps(maps, aliases, "map_*.qc")
     for folder in args.folder or []:
-        add_maps(maps, os.path.join(folder, "map_*.qc"))
+        add_maps(maps, aliases, os.path.join(folder, "map_*.qc"))
     with open(args.list_file, "w", encoding="ascii") as list_file:
         print("\n".join(maps), file=list_file)
+    with open(args.alias_file, "w", encoding="ascii") as alias_file:
+        entries = [f"{mapname} {' '.join(ali)}" for mapname, ali in aliases.items()]
+        print("\n".join(entries), file=alias_file)
     if args.verbose:
         print(f"Written '{args.list_file}' with {len(maps)} entries")
+        print(f"Written '{args.alias_file}' with {len(aliases)} entries")
+
+
+def load_map_aliases(path: str, map_names: list[str]) -> dict[str, list[str]]:
+    """Loads map aliases from file, extends @map_names with any matching aliases,
+    and returns the dict with the aliases."""
+    result = {}
+    with open(path, encoding="ascii") as alias_data:
+        for line in alias_data.readlines():
+            parts = line.strip().split()
+            if not parts:
+                continue
+            map_name, *aliases = parts
+            for alias in aliases:
+                if alias in map_names:
+                    print(f"WARNING: ignoring aliased '{alias}' because already in map names",
+                        file=sys.stderr)
+            aliases = [alias for alias in aliases if alias not in map_names]
+            if not aliases or map_name not in map_names:
+                continue
+            result[map_name] = aliases
+            map_names.extend(aliases)
+    return result
 
 
 def generate_qc_source(args: argparse.Namespace) -> None:
-    """Generates a QC source file ../{GENERATE_QC} for maps listed in args.list_file."""
+    """Generates a QC source file ../{GENERATE_QC} for maps listed in args.list_file
+    and aliases in args.alias_file."""
     with open(args.list_file, encoding="ascii") as list_file:
-        map_files = [line.strip() for line in list_file.readlines()]
-    map_names = [os.path.basename(map_file)[4:][:-3].lower() for map_file in map_files]
+        map_files = [line.strip() for line in list_file.readlines() if line.strip()]
+    map_names = [mapname_from_path(map_file) for map_file in map_files]
+    aliases = load_map_aliases(args.alias_file, map_names)
+    reverse_aliases = {
+        rev_ali: mapname
+        for mapname, alis in aliases.items()
+        for rev_ali in alis
+    }
     if args.verbose:
         print(f"Read {len(map_files)} entries from '{args.list_file}'")
+        print(f"Read {len(aliases)} entries from '{args.alias_file}'")
 
     with open(os.path.join("..", GENERATE_QC), "w", encoding="ascii") as qc_file:
         print('// This file has been generated with generate_maplist.py\n', file=qc_file)
@@ -66,7 +129,7 @@ def generate_qc_source(args: argparse.Namespace) -> None:
         print("", file=qc_file)
 
         print("void() SprintMaps =\n{", file=qc_file)
-        map_names_sorted = sorted(map_names, key=os.path.basename)
+        map_names_sorted = sorted(map_names)
         for i in range(0, len(map_names_sorted), COLUMNS):
             row = map_names_sorted[i:i+COLUMNS]
             row_text = " ".join(f"{m_name:>{COL_WIDTH}}" for m_name in row)
@@ -79,10 +142,11 @@ def generate_qc_source(args: argparse.Namespace) -> None:
 
         print("float() LoadWaypoints =\n{", file=qc_file)
         for map_name in map_names:
+            actual_map = reverse_aliases.get(map_name, map_name)
             print(
                 f"""\tif (mapname == "{map_name}")
 \t{{
-\t\tmap_{map_name}();
+\t\tmap_{actual_map}();
 \t\treturn TRUE;
 \t}}""", file=qc_file)
         print("\treturn FALSE;\n};\n", file=qc_file)
@@ -95,7 +159,7 @@ def generate_qc_source(args: argparse.Namespace) -> None:
         print('\treturn "";\n};', file=qc_file)
 
         if args.verbose:
-            print(f"Written '{qc_file.name}' with {len(map_files)} entries")
+            print(f"Written '{qc_file.name}' with {len(map_files) + len(reverse_aliases)} entries")
 
 
 def wipe_obsolete_descriptions(line: str) -> Tuple[bool, str]:
@@ -111,9 +175,9 @@ def wipe_obsolete_descriptions(line: str) -> Tuple[bool, str]:
             new_chunks.append(chunk)
             continue
         old_bits = int(parts.group(3))
-        # Clear all bits below 256. In theory we could preserve value 2
-        # because it still represents a forced water jump, but in practice
-        # I have found no waypoint files using this value.
+        # Clear all bits below 256. In theory we could preserve value 2 which used to
+        # represent WATERJUMP_, and the new FOCUS_PATH may be useful in the same situation,
+        # but in practice I have found no waypoint files using this value.
         new_bits = old_bits & ~0xFF
         if old_bits == new_bits:
             new_chunks.append(chunk)
@@ -161,22 +225,33 @@ def transform_qc_files(args: argparse.Namespace) -> None:
 
 def main():
     """Script entry point"""
-    parser = argparse.ArgumentParser(
-        description=("Sets up source files for building Quake FrogBot with waypoint files for a set of maps. "
+    parser = argparse.ArgumentParser(description=(
+        "Sets up source files for building Quake FrogBot with waypoint files for a set of maps. "
         "The map QuakeC files must be in the working directory, and be named 'map_mapname.qc'. "
-        "Inside the QC file, there must be a definition for void() map_mapname.qc, and mapname must be lowercase."))
+        "Inside the QC file, there must be a definition for void() map_mapname.qc, and "
+        "mapname must be lowercase.")
+    )
     parser.add_argument('-v', '--verbose', action='store_true',
-                        help="verbose output.")
+        help="verbose output.")
     parser.add_argument('-l', '--make_list', action='store_true',
-                        help="generate a maplist.txt file with all waypoint source files in this directory. You can edit this file afterwards to select only the desired maps. Any existing file will be overwritten.")
+        help="generate a maplist.txt file with all waypoint source files in this directory. "
+             "You can edit this file afterwards to select only the desired maps. "
+             "Any existing file will be overwritten.")
     parser.add_argument('-g', '--generate', action='store_true',
-                        help=f"generate the '{GENERATE_QC}' file one directory level up. If there is a 'maplist.txt' file, this will be used; otherwise a new one will be generated as if -l was used.")
+        help=f"generate the '{GENERATE_QC}' file one directory level up. "
+             "If there is a 'maplist.txt' file, this will be used; otherwise "
+             "a new one will be generated as if -l was used.")
     parser.add_argument('-t', '--transform', action='store_true',
-                        help="convert map sources from old N('vector') to new N(x,y,z) format. Same remark about map list file as with -g.")
+        help="convert map sources from old N('vector') to new N(x,y,z) format. "
+             "Same remark about map list file as with -g.")
     parser.add_argument('-f','--list_file', type=str, default='maplist.txt',
-                        help="use a custom file name instead of 'maplist.txt'.")
+        help="use a custom file name instead of 'maplist.txt'.")
+    parser.add_argument('-a','--alias_file', type=str, default='mapaliases.txt',
+        help="use a custom aliases file name instead of 'mapaliases.txt'. "
+             "If the QC file for 'mapname' contains a line '// ALIASES ali1 ali2', then "
+             "the aliases file will contain a line 'mapname ali1 ali2'.")
     parser.add_argument('-d','--folder', type=str, nargs="+",
-                        help="also include maps in these subdirectories.")
+        help="also include maps in these subdirectories.")
     args = parser.parse_args()
 
     if args.verbose:
