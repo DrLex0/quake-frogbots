0d874f0914c533831526e0afd7dd55542a842639
Alexander Thomas <doctor.lex@gmail.com>
2026-09-12 15:53:05 +0200

    Enable the most useful debug flags for waypoint build by default

--- src/build-waypoint.sh
+++ src/build-waypoint.sh
@@ -2,7 +2,9 @@
 # Convenience script for building the waypoint tool and optionally deploying it
 
 mkdir -p ../Release/waypoint
-fteqcc.bin -DWAYPOINT_BUILD=1 -O3 -srcfile progs-waypoint.src "$@"
+# Enable rather essential debug log flags; see waypoint documentation for others.
+# Thanks to "$@", you can also just invoke this script with extra parameters.
+fteqcc.bin -DWAYPOINT_BUILD=1 -DDEBUG_TOUCH -DDEBUG_HAZD -O3 -srcfile progs-waypoint.src "$@"
 
 # Instant deploy: set TARGET_DIR to the desired game dir within your netquake install dir
 # (create it first in the same directory as the id1 dir).
